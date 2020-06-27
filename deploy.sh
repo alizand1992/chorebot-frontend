1ed058c092e8fa8f753ae74da15907d0a7fe97b9
Ali Zand <ali.zand9224@gmail.com>
2020-06-27 15:14:40 -0700

    CBF-4 Make Travis autopush to the prod server

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+git config --global push.default matching # we only want to push one branch — master
+git remote add deploy ssh://deploy@45.77.1.73:22/var/www/chorebot-frontend
+ssh-keyscan -H 45.77.1.73 >> ~/.ssh/known_hosts
+git push deploy master
+ssh deploy@45.77.1.73 'export PATH="$PATH:/home/deploy/.nvm/versions/node/v12.18.1/bin/" && cd /var/www/chorebot-frontend && npm install && npm run build && sudo service nginx restart'
