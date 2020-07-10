636c46077f2858bca3b3309a97692f146cd9423a
Ali Zand <ali.zand9224@gmail.com>
2020-07-09 20:30:15 -0700

    Try pulling directly from the repo

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-git config --global push.default matching # we only want to push one branch — master
-git remote add deploy ssh://deploy@45.77.1.73:22/var/www/chorebot-frontend
+#git config --global push.default matching # we only want to push one branch — master
+#git remote add deploy ssh://deploy@45.77.1.73:22/var/www/chorebot-frontend
 ssh-keyscan -H 45.77.1.73 >> ~/.ssh/known_hosts
-git push deploy master
+#git push deploy master
+ssh deploy@45.77.1.73 'cd /var/www/chorebot-frontend && git checkout . && git clean -fd && git pull --prune'
 ssh deploy@45.77.1.73 'export PATH="$PATH:/home/deploy/.nvm/versions/node/v12.18.1/bin/" && cd /var/www/chorebot-frontend && npm install && npm run build && sudo service nginx restart'
