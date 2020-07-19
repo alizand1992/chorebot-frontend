e13dc989f9fe171d0f74183d8463f2f8f3e737cd
Ali Zand <ali.zand9224@gmail.com>
2020-07-18 19:51:32 -0700

    CBF-20 Create component for loading. Replace CircularProgress with new Loading component

--- src/App.js
+++ src/App.js
@@ -2,9 +2,8 @@ import React, { Suspense } from 'react';
 
 import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 
-import CircularProgress from '@material-ui/core/CircularProgress';
-
 import TopMenu from './components/TopMenu';
+import Loading from './components/Common/Loading';
 
 function App() {
   const SignIn = React.lazy(() => import('./components/User/SignIn'));
@@ -19,23 +18,23 @@ function App() {
         <br />
         <Switch>
           <Route path="/user/sign_in">
-            <Suspense fallback={<CircularProgress />}>
+            <Suspense fallback={<Loading />}>
               <SignIn />
             </Suspense>
           </Route>
           <Route path="/user/sign_up">
-            <Suspense fallback={<CircularProgress />}>
+            <Suspense fallback={<Loading />}>
               <SignUp />
             </Suspense>
           </Route>
         </Switch>
       </Router>
       <br /><br />
-      <Suspense fallback={<CircularProgress />}>
+      <Suspense fallback={<Loading />}>
         <Form />
       </Suspense>
       <br />
-      <Suspense fallback={<CircularProgress />}>
+      <Suspense fallback={<Loading />}>
         <Task />
       </Suspense>
     </div>
